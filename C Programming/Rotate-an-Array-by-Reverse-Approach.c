a57d2e2871b9712b9dcbd267857d9b5f520562e9
KamalveerStarkyDev <kamalveer356@gmail.com>
2024-07-02 22:36:33 +0530

    add two programs with different approaches to rotate an array

---
+++ C Programming/Rotate-an-Array-by-Reverse-Approach.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <ctype.h>
+
+// Function to reverse a portion of the array
+void reverseArray(int arr[], int start, int end)
+{
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\n");
+}
+
+void rotateArray(int arr[], int length, int shift, char shift_type)
+{
+    shift %= length; // Bring shift within range of array length
+    if (shift_type == 'r' || shift_type == 'R')
+    {
+        shift = length - shift; // Convert right rotation to left rotation equivalent
+    }
+
+    // Use reversal algorithm to perform rotation in-place
+    reverseArray(arr, 0, shift - 1);
+    reverseArray(arr, shift, length - 1);
+    reverseArray(arr, 0, length - 1);
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    int shift_value;
+    char type_of_shift;
+
+    printf("The original array is:\n");
+    printArray(arr, length);
+
+    printf("Enter the value by which you want to shift: ");
+    scanf("%d", &shift_value);
+
+    printf("Enter (l or L) for left shift and (r or R) for right shift: ");
+    scanf(" %c", &type_of_shift);
+
+    // Validate shift type input
+    while (type_of_shift != 'l' && type_of_shift != 'L' && type_of_shift != 'r' && type_of_shift != 'R')
+    {
+        printf("Please enter a valid shift type (l or r): ");
+        scanf(" %c", &type_of_shift);
+    }
+
+    rotateArray(arr, length, shift_value, type_of_shift);
+    printf("\nThe array after shifting:\n");
+    printArray(arr, length);
+
+    return 0;
+}
+
+/*
+SAMPLE INPUT:
+The original array is:
+1 2 3 4 5
+
+Enter the value by which you want to shift: 2
+Enter (l or L) for left shift and (r or R) for right shift: l
+
+SAMPLE OUTPUT:
+The array after shifting:
+3 4 5 1 2
+
+ITERATION:
+array = 1 2 3 4 5
+shift = 2
+shift_type = l
+reverse array from index 0 to 1: 2 1 3 4 5
+reverse array from index 2 to 4(length - 1): 2 1 5 4 3
+reverse array from index 0 to 4(length - 1): 3 4 5 1 2
+output = 3 4 5 1 2
+*/
---
+++ C Programming/Rotate-an-Array.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void printArray(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\n");
+}
+
+int *rotateArray(int arr[], int length, int shift, char shift_type)
+{
+
+    char type_of_shift = tolower(shift_type);
+
+    // Fixed the while loop condition
+    while (type_of_shift != 'l' && type_of_shift != 'r')
+    {
+        printf("Please enter a valid shift type: ");
+        scanf(" %c", &type_of_shift); // Note the space before %c to eat the newline
+        type_of_shift = tolower(type_of_shift);
+    }
+
+    shift %= length; // if shift = 7 and length = 5 then it'll make shift 2 (i.e. in range) as shifting by 2 or 7 in this case of 5 elements will be equivalent.
+
+    int temp[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        if (type_of_shift == 'r')
+        {
+            temp[i] = arr[(i - shift + length) % length]; // for i = 0, shift = 2 and length = 5 ==> (0 + 2 + 5) % 5 = 2. so, 2nd element will be placed at 0th index
+        }
+        else
+        {
+            temp[i] = arr[(i + shift + length) % length];
+        }
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = temp[i]; // copying temp array to passed array as you can't return local variables
+    }
+
+    return arr;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    int shift_value;
+    char type_of_shift;
+
+    printf("The original array is:\n");
+    printArray(arr, length);
+
+    printf("Enter the value by which you want to left shift: ");
+    scanf("%d", &shift_value);
+
+    printf("Enter (l or L) for left shift and (r or R) for right shift: ");
+    scanf(" %c", &type_of_shift);
+
+    int *rotated_array = rotateArray(arr, length, shift_value, type_of_shift);
+    printf("\nThe array after shifting is:\n", shift_value);
+    printArray(rotated_array, length);
+
+    return 0;
+}
+
+/*
+SAMPLE INPUT:
+The original array is:
+1 2 3 4 5
+
+Enter the value by which you want to left shift: 2
+Enter (l or L) for left shift and (r or R) for right shift: r
+
+SAMPLE OUTPUT:
+The array after shifting is:
+4 5 1 2 3
+*/
