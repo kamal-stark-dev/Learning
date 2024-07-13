fb2225420eee91ae68a30f34363dc123f54fd637
KamalveerStarkyDev <kamalveer356@gmail.com>
2024-07-13 22:31:03 +0530

    add print range code with args to main

---
+++ C Programming/Argument Passing in main()/Print-Range.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s <start> <end>\n", argv[0]);
+        exit(1);
+    }
+
+    int start, end;
+
+    start = atoi(argv[1]);
+    end = atoi(argv[2]);
+
+    for (int i = start; i <= end; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
