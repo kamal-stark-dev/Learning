fc36e9f3994a079ddb2294b8c5a9d3a4dd7d2dca
KamalveerStarkyDev <kamalveer356@gmail.com>
2025-02-20 22:27:06 +0530

    add styling to todo  list

--- Python/Automation/03-tobenamed.py
+++ Python/Automation/03-tobenamed.py
@@ -33,6 +33,10 @@ try:
     while True:
         x, y = pyautogui.position()
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        pixelColor = pyautogui.screenshot().getpixel((x, y))
+        positionStr += ' RGB: (' + str(pixelColor[0]).rjust(3) + ', '
+        positionStr += str(pixelColor[1]).rjust(3) + ', '
+        positionStr += str(pixelColor[2]).rjust(3) + ')'
         print(positionStr, end='')
         print('\b' * len(positionStr), end='', flush=True) # try using this without flush
         # Note: once a new line is printed you can't erase any characters before it
@@ -41,6 +45,7 @@ except KeyboardInterrupt:
 
 # the coordinates where you stopped will be printed
 print('X:', str(x).rjust(4), ' Y:', str(y).rjust(4))
+print(pixelColor)
 '''
 
 # clicking the mouse
@@ -59,4 +64,27 @@ while distance > 0:
     pyautogui.dragRel(-distance, 0, duration=0.2) # move left
     distance -= 20
     pyautogui.dragRel(0, -distance, duration=0.2) # move up
+'''
+
+# time.sleep(3)
+# pyautogui.scroll(400)
+
+copilotImage = pyautogui.locateOnScreen('./assets/copilot.png')
+print(copilotImage)
+print(pyautogui.center(copilotImage))
+# pyautogui.click(pyautogui.center(copilotImage))
+
+# controlling the keyboard
+'''
+pyautogui.click(1800, 900) # click on terminal
+pyautogui.typewrite('git status')
+pyautogui.press('enter')
+'''
+
+time.sleep(3)
+pyautogui.typewrite(['#', 'a', 'b', 'left', 'left', 'x', 'y'], interval=0.1)
+
+# keydown and keyup
+'''
+pyautogui.keyDown('shift'); pyautogui.press('4'); pyautogui.keyDown('shift')
 '''
