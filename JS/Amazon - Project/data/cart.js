e89e9b1785c0b354a9cf2ebb31a6a86466d85ef1
kamal-stark-dev <kamalveer356@gmail.com>
2025-03-02 09:27:56 +0530

    save cart in localStorage

--- JS/Amazon - Project/data/cart.js
+++ JS/Amazon - Project/data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem("cart")) ?? [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -8,6 +8,10 @@ export let cart = [
     quantity: 1,
   },
 ];
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   const quantitySelect = document.querySelector(
@@ -31,6 +35,8 @@ export function addToCart(productId) {
       quantity: selectedQuantity,
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -43,4 +49,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
