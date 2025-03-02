c3fc69be5772a40018fb0f42c18f8639966b03ce
kamal-stark-dev <kamalveer356@gmail.com>
2025-03-02 09:20:48 +0530

    remove the HTML of removed cartItem using DOM

--- JS/Amazon - Project/scripts/checkout.js
+++ JS/Amazon - Project/scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
     <div class="cart-item-details-grid">
@@ -97,5 +97,8 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     const productId = link.dataset.productId;
     // remove the cartItem from the cart array
     removeFromCart(productId);
+
+    // removing the HTML for the removed item
+    document.querySelector(`.js-cart-item-container-${productId}`).remove();
   });
 });
