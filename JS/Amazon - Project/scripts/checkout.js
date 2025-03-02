249b8625a0a2cb04b42fe52a71e89530249717b7
kamal-stark-dev <kamalveer356@gmail.com>
2025-03-02 09:15:39 +0530

    removing cartItems from cart when click 'delete'

--- JS/Amazon - Project/scripts/checkout.js
+++ JS/Amazon - Project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -38,7 +38,9 @@ cart.forEach((cartItem) => {
                 <span class="update-quantity-link link-primary">
                     Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+                  matchingProduct.id
+                }">
                     Delete
                 </span>
             </div>
@@ -89,3 +91,11 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    // remove the cartItem from the cart array
+    removeFromCart(productId);
+  });
+});
