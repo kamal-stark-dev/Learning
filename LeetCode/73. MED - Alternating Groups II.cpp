d564235e61fe9dc80e8b5e34b649a81818006703
kamal-stark-dev <kamalveer356@gmail.com>
2025-03-09 10:06:23 +0530

    solved with no extra space

--- LeetCode/73. MED - Alternating Groups II.cpp
+++ LeetCode/73. MED - Alternating Groups II.cpp
@@ -19,10 +19,25 @@ int numberOfAlternatingGroups(vector<int>& colors, int k) {
     return count;
 }
 
+int numberOfAlternatingGroups_witnoutExtraSpace(vector<int>& colors, int k) {
+    int n = colors.size();
+    int left = 0, count = 0;
+    for (int right = 0; right < n + k - 1; right++) {
+        if (right > 0 && colors[right % n] == colors[(right - 1) % n]) {
+            left = right;
+        }
+        if (right - left + 1 >= k) {
+            count ++;
+        }
+    }
+    return count;
+}
+
 int main(void) {
     vector<int> colors = {0, 1, 0, 1, 0};
     int k = 3;
 
-    cout << "There are " << numberOfAlternatingGroups(colors, k) << " alternating groups of size " << k << ".\n";
+    // cout << "There are " << numberOfAlternatingGroups(colors, k) << " alternating groups of size " << k << ".\n";
+    cout << "There are " << numberOfAlternatingGroups_witnoutExtraSpace(colors, k) << " alternating groups of size " << k << ".\n";
     return 0;
 }
