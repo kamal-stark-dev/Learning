516bd02ff24d8dfa8de58040fdfdd12e7f5a1f59
kamal-stark-dev <kamalveer356@gmail.com>
2025-03-13 21:39:22 +0530

    add time complexities in detail

--- LeetCode/79. HARD - Largest Rectangle in Histogram.cpp
+++ LeetCode/79. HARD - Largest Rectangle in Histogram.cpp
@@ -11,7 +11,7 @@ int largestAreaRectangle(vector<int>& heights) {
     stack<int> s;
 
     vector<int> right(size, 0);
-    for (int i = size - 1; i >= 0; i--) {
+    for (int i = size - 1; i >= 0; i--) { // O(n)
         while(!s.empty() && heights[s.top()] >= heights[i]) s.pop();
         right[i] = s.empty() ? -1 : s.top();
         s.push(i);
@@ -20,14 +20,14 @@ int largestAreaRectangle(vector<int>& heights) {
     // remove any elements from the stack
     while (!s.empty()) s.pop();
 
-    vector<int> left(size, 0);
+    vector<int> left(size, 0); // O(n)
     for (int i = 0; i < size; i++) {
         while(!s.empty() && heights[s.top()] >= heights[i]) s.pop();
         left[i] = s.empty() ? -1 : s.top();
         s.push(i);
     }
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) { // O(n)
         int width = right[i] - left[i] - 1;
         int currArea = heights[i] * width;
         maxArea = max(currArea, maxArea);
@@ -46,5 +46,5 @@ int main(void) {
 }
 /*
 Time Complexity: O(n)
-Space Complexity: O(n)
+Space Complexity: O(n) -> stack, left and right
 */
