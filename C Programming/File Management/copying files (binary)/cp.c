0a7fb0d75a9f5b4e381e4d8e313854f9115aba3d
kamal-stark-dev <kamalveer356@gmail.com>
2025-04-03 20:08:59 +0530

    add error handling in copy files

--- C Programming/File Management/copying files (binary)/cp.c
+++ C Programming/File Management/copying files (binary)/cp.c
@@ -5,8 +5,25 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
+    if (argc != 3)
+    {
+        printf("Usage: %s <source_file> <destination_file>\n", argv[0]);
+        return 1;
+    }
+
     FILE *src = fopen(argv[1], "rb");
+    if (src == NULL)
+    {
+        perror("Error opening source file.");
+        return 2;
+    }
+
     FILE *dst = fopen(argv[2], "wb");
+    if (dst == NULL)
+    {
+        perror("Error opening destination file.");
+        return 2;
+    }
 
     BYTE b;
 
@@ -17,6 +34,10 @@ int main(int argc, char *argv[])
 
     fclose(src);
     fclose(dst);
+
+    printf("File copied succesfully!\n");
+
+    return 0;
 }
 
 // usage: ./cp.c source.txt destination.txt (you can also copy images with it)
