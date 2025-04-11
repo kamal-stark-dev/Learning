112c5a2a2b2377e987f92a5ea66a4c9fd0a87dbb
kamal-stark-dev <kamalveer356@gmail.com>
2025-04-11 15:37:09 +0530

    add union of sorted arrays

---
+++ LeetCode/97. Union of Two Sorted Arrays.cpp
@@ -0,0 +1,51 @@
+// Union of Two Sorted Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findUnion(vector<int> a, vector<int> b) {
+    int n = a.size(), m = b.size();
+    int i = 0, j = 0;
+    vector<int> unionArr;
+
+    while (i < n && j < m) {
+        if (a[i] <= b[j]) {
+            if (unionArr.size() == 0 || unionArr.back() != a[i]) {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else {
+            if (unionArr.size() == 0 || unionArr.back() != b[j]) {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+
+    while (i < n) {
+        if (unionArr.size() == 0 || unionArr.back() != a[i]) {
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+
+    while (j < m) {
+        if (unionArr.size() == 0 || unionArr.back() != b[j]) {
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+
+    return unionArr;
+}
+
+int main(void) {
+    vector<int> a = {1, 1, 2, 3, 4, 5};
+    vector<int> b = {2, 3, 4, 4, 5, 6};
+
+    vector<int> unionArr = findUnion(a, b);
+
+    for (int n : unionArr) cout << n << " ";
+    return 0;
+}
