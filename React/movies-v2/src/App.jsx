6ef2921e038279090faca8c206c480831f391bde
kamal-stark-dev <kamalveer356@gmail.com>
2025-07-11 07:36:29 +0530

    configured tailwind and add search UI

--- React/movies-v2/src/App.jsx
+++ React/movies-v2/src/App.jsx
@@ -1,62 +1,24 @@
-import { useState, useEffect } from "react";
-
-const Card = ({ title, rating, isBadass, actors }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been: ${hasLiked ? "Liked" : "Disliked"}`);
-  }, [hasLiked]);
-
-  useEffect(() => {
-    console.log("Card Rendered!");
-  }, []); // will only render when the component is first made
-
-  return (
-    <div
-      className="card"
-      onClick={() => setCount((prevState) => prevState + 1)}
-    >
-      <h2>
-        {title} - {count}
-      </h2>
-      <p>Rating: {rating} </p>
-      <p>Badass: {isBadass ? "Yes" : "No"}</p>
-      <p>Star Cast: {actors}</p>
-      <button onClick={() => setHasLiked((prevHasLiked) => !prevHasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  );
-};
+import React, { useState } from "react";
+import Search from "./components/Search.jsx";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <>
-      <h2 style={{ fontSize: "32px", marginBottom: "48px" }}>
-        Functional Arrow Component
-      </h2>
-      <div className="card-container">
-        <Card
-          title="Kesari: Chapter 2"
-          rating={4.1}
-          isBadass={false}
-          actors={["Akshay Kumar", "R. Madhavan", "Ananya Pandey"]}
-        ></Card>
-        <Card
-          title="KGF Chapter 2"
-          rating={4.8}
-          isBadass={true}
-          actors={["Yash", "Sanjay Dutt", "Raveena Tandon", "Srinidhi Shetty"]}
-        ></Card>
-        <Card
-          title="Bajrangi Bhaijaan"
-          rating={4.6}
-          isBadass={false}
-          actors={["Salman Khan", "Kareena Kapoor", "Nawazuddin Siddiqui"]}
-        ></Card>
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without Hastle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
-    </>
+    </main>
   );
 };
+
 export default App;
---
+++ React/movies-v2/src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="./search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
