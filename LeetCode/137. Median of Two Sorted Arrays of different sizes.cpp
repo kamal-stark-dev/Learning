955887260cc5c91de07328b22430a346293537a0
kamal-stark-dev <kamalveer356@gmail.com>
2025-07-16 06:23:19 +0530

    add binary search logic on median of two sorted arrays

--- LeetCode/137. Median of Two Sorted Arrays of different sizes.cpp
+++ LeetCode/137. Median of Two Sorted Arrays of different sizes.cpp
@@ -27,7 +27,7 @@ double findMedian_Brute(vector<int>& nums1, vector<int>& nums2) {
     }
 }
 
-double findMedian(vector<int>& nums1, vector<int>& nums2) {
+double findMedian_Better(vector<int>& nums1, vector<int>& nums2) {
     int n1 = nums1.size(), n2 = nums2.size();
     int medianIdx = (n1 + n2) / 2 + 1;
     double median, prevMedian = -1;
@@ -62,11 +62,46 @@ double findMedian(vector<int>& nums1, vector<int>& nums2) {
     return median;
 }
 
+// optimal methods is to use binary search for the same
+double findMedian(vector<int>& nums1, vector<int>& nums2) {
+    int n1 = nums1.size(), n2 = nums2.size();
+    if (n1 > n2) return findMedian(nums2, nums1);
+
+    int leftSize = (n1 + n2 + 1) / 2; // also handles the odd lengths
+    int left = 0, right = n1;
+    while (left <= right) { // O(log R), R -> 0 to min(n1, n2)
+        int mid1 = (left + right) / 2;
+        int mid2 = leftSize - mid1;
+
+        int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+        if (mid1 < n1) r1 = nums1[mid1];
+        if (mid2 < n2) r2 = nums2[mid2];
+        if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+        if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1) {
+            if ((n1 + n2) % 2 == 0) {
+                return (double) (max(l1, l2) + min(r1, r2)) / 2.0;
+            }
+            else {
+                return max(l1, l2);
+            }
+        }
+        else if (l1 > r2) right = mid1 - 1;
+        else left = mid1 + 1;
+    }
+    return 0;
+}
+
 int main(void) {
     vector<int> nums1 = {2, 4, 6};
-    vector<int> nums2 = {1, 3, 5};
+    vector<int> nums2 = {1, 3};
 
     cout << "Median of two sorted arrays is " << findMedian(nums1, nums2) << "\n";
 
     return 0;
 }
+
+/*
+Time Complexity (Optimal): O(log min(n1, n2))
+*/
