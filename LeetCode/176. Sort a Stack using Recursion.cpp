b4e4b8842624c1b331700f22b98d3eaacdb595f8
kamal-stark-dev <kamalveer356@gmail.com>
2025-08-01 21:22:10 +0530

    add sort stack using recursion

---
+++ LeetCode/176. Sort a Stack using Recursion.cpp
@@ -0,0 +1,52 @@
+// We need to sort a stack but using recurison (only)
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void printStack(stack<int> st) {
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+}
+
+void insertSorted(stack<int>& st, int element) {
+    if (st.empty() || st.top() >= element) {
+        st.push(element);
+    }
+    else {
+        int temp = st.top();
+        st.pop();
+        insertSorted(st, element);
+        st.push(temp);
+    }
+}
+
+void sortStack(stack<int>& st) {
+    if (!st.empty()) {
+        int top = st.top();
+        st.pop();
+        sortStack(st);
+        insertSorted(st, top);
+    }
+    return;
+}
+
+int main(void) {
+    stack<int> st;
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(1);
+
+    sortStack(st); // O(n ^ 2)
+    printStack(st); // O(n)
+
+    return 0;
+}
+
+/*
+Time Complexity: O(n ^ 2), because each element may have to move all others to get inserted at correct position.
+Space Complexity: O(n), for recursion stack.
+*/
