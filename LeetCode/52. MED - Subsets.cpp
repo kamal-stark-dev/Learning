66b5a0269917d9cd415d5c7f5396173fad3d1ac3
kamal-stark-dev <kamalveer356@gmail.com>
2025-08-04 21:47:24 +0530

    add subsets iterative solution

--- LeetCode/52. MED - Subsets.cpp
+++ LeetCode/52. MED - Subsets.cpp
@@ -28,10 +28,27 @@ vector<vector<int>> subsets(vector<int>& nums) {
     return subsets;
 }
 
+vector<vector<int>> subsets_iterative(vector<int>& nums) {
+    vector<vector<int>> subsets;
+    int n = nums.size();
+    long long twoPowerN = 1 << n;
+
+    for (int i = 0; i < twoPowerN; i++) {
+        vector<int> temp;
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                temp.push_back(nums[j]);
+            }
+        }
+        subsets.push_back(temp);
+    }
+    return subsets;
+}
+
 int main(void) {
     vector<int> nums = {1, 2, 3};
 
-    vector<vector<int>> ans = subsets(nums);
+    vector<vector<int>> ans = subsets_iterative(nums);
 
     for (int i = 0; i < ans.size(); i++) {
         for (int j = 0; j < ans[i].size(); j++) {
@@ -42,3 +59,8 @@ int main(void) {
 
     return 0;
 }
+
+/*
+Time Complexity: O(2 ^ n * n)
+Space Complexity: O(2 ^ n * n) -> to store subsets
+*/
