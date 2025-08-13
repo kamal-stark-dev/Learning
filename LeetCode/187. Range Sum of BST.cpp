14ab2e8cfb56d160c2d1af5fe967a0a1508cbca8
kamal-stark-dev <kamalveer356@gmail.com>
2025-08-13 18:42:21 +0530

    add range sum BST pruning - optimal

--- LeetCode/187. Range Sum of BST.cpp
+++ LeetCode/187. Range Sum of BST.cpp
@@ -38,17 +38,33 @@ int rangeSumBST_2(Node* root, int low, int high) {
     return rangeSumBST_2(root->left, low, high) + rangeSumBST_2(root->right, low, high);
 }
 
+// Most Optimal Way - Pruning
+
+int rangeSumBST_Pruning(Node* root, int low, int high) {
+    if (!root) return 0;
+    if (root->val < low) {
+        // prune the left subtree
+        return rangeSumBST_Pruning(root->right, low, high);
+    }
+    if (root->val > high) {
+        // prune the right subtree
+        return rangeSumBST_Pruning(root->left, low, high);
+    }
+
+    return root->val + rangeSumBST_Pruning(root->left, low, high) + rangeSumBST_Pruning(root->right, low, high);
+}
+
 int main(void) {
     Node* root = new Node(10);
     root->left = new Node(5);
     root->right = new Node(15);
     root->left->left = new Node(3);
-    root->left->left = new Node(7);
+    root->left->right = new Node(7);
     root->right->right = new Node(18);
 
     int low = 7, high = 15;
 
-    cout << "The range sum is " << rangeSumBST_2(root, low, high) << ".\n";
+    cout << "The range sum is " << rangeSumBST_Pruning(root, low, high) << ".\n";
 
     return 0;
 }
