42381c6ecafe1b9b67c49205ce4289f7b75d88f8
kamal-stark-dev <kamalveer356@gmail.com>
2025-08-17 17:07:55 +0530

    fixed stack reverse problem

--- LeetCode/177. Reverse a Stack using Recursion.cpp
+++ LeetCode/177. Reverse a Stack using Recursion.cpp
@@ -12,22 +12,35 @@ void printStack(stack<int> st) {
     cout << '\n';
 }
 
+void insert(stack<int>& st, int element) {
+    stack<int> temp;
+
+    while(!st.empty()) {
+        temp.push(st.top());
+        st.pop();
+    }
+    st.push(element);
+    while(!temp.empty()) {
+        st.push(temp.top());
+        temp.pop();
+    }
+}
+
 void reverseStack(stack<int>& st) {
     if (st.empty()) return;
 
     int temp = st.top();
     st.pop();
     reverseStack(st);
-    st.push(temp);
-
+    insert(st, temp);
 }
 
 int main(void) {
     stack<int> st;
-    st.push(1);
-    st.push(2);
-    st.push(3);
     st.push(4);
+    st.push(3);
+    st.push(2);
+    st.push(1);
 
     cout << "Stack: ";
     printStack(st);
