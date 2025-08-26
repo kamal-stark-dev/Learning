8ca505f2ba44c835c04fa6c2573d0a3d58b5a641
kamal-stark-dev <kamalveer356@gmail.com>
2025-08-26 07:42:33 +0530

    add permutations with other approaches

--- LeetCode/54. MED - Permutations.cpp
+++ LeetCode/54. MED - Permutations.cpp
@@ -19,11 +19,52 @@ void getPerms(vector<int>& nums, int idx, vector<vector<int>>& permutations) {
     }
 }
 
+vector<vector<int>> permute_2(vector<int>& nums) {
+    // base case
+    if (nums.size() == 0) {
+        return { { } };
+    }
+
+    int last = nums[nums.size() - 1];
+    nums.pop_back();
+
+    vector<vector<int>> perms = permute_2(nums);
+    vector<vector<int>> res;
+
+    for (auto permutation: perms) {
+        for (int i = 0; i <= permutation.size(); i++) {
+            vector<int> copy(permutation);
+            copy.insert(copy.begin() + i, last);
+            res.push_back(copy);
+        }
+    }
+    return res;
+}
+
 vector<vector<int>> permute(vector<int>& nums) {
     vector<vector<int>> permutations;
     getPerms(nums, 0, permutations); // calling the helper function
 
     return permutations;
+}
+
+// Linear Approach
+vector<vector<int>> permute_3(vector<int>& nums) {
+    vector<vector<int>> perms = { { } };
+
+    for (int n: nums) {
+        vector<vector<int>> new_perms;
+        for (auto p: perms) {
+            for (int i = 0; i <= p.size(); i++) {
+                vector<int> copy(p);
+                copy.insert(copy.begin() + i, n);
+                new_perms.push_back(copy);
+            }
+        }
+        perms = new_perms;
+    }
+
+    return perms;
 }
 
 int main(void) {
@@ -35,7 +76,7 @@ int main(void) {
     NOTE: for a `n` elements we have `n!` permutations for the same (n = 3, n! = 3 x 2 x 1 = 6)
     */
 
-    vector<vector<int>> permutations = permute(nums);
+    vector<vector<int>> permutations = permute_3(nums);
 
     for (vector<int> permutation : permutations) {
         for (int num : permutation) {
