22061f8cddaf4a313a76221bd2e36fc1a34880a4
kamal-stark-dev <kamalveer356@gmail.com>
2025-09-19 17:46:50 +0530

    add min bit flips to convert numbers

--- React/movies-v2/src/App.jsx
+++ React/movies-v2/src/App.jsx
@@ -1,8 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./components/Search.jsx";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      throw new Error("Failed to fetch movies.");
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setErrorMessage("Error fetching movies. Please try again later.");
+    }
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <main>
@@ -14,8 +41,12 @@ const App = () => {
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without Hastle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
   );
