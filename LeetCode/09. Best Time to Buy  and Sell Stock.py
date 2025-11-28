706795e8d787fb60807007b02d65e819419e279f
kamal-stark-dev <kamalveer356@gmail.com>
2025-11-28 14:09:18 +0530

    revisited best time to buy and sell stock

--- LeetCode/09. Best Time to Buy  and Sell Stock.py
+++ LeetCode/09. Best Time to Buy  and Sell Stock.py
@@ -1,5 +1,18 @@
 # 121. Best Time to Buy and Sell Stock
 
+def maxProfit_2Pointers(prices: list[int]) -> int:
+    left, right = 0, 1
+    maxP = 0
+
+    while right < len(prices):
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            maxP = max(maxP, profit)
+        else:
+            left = right
+        right += 1
+    return maxP
+
 def maxProfit(prices: list[int]) -> int:
 
     """ Brute Force - O(n^2)
@@ -11,36 +24,18 @@ def maxProfit(prices: list[int]) -> int:
     return Max
     """
 
-    """ Time - O(n) and Space - O(1)
-    left, right = 0, 1
-    profit_so_far = 0
-
-    while right < len(prices):
-        if prices[right] < prices[left]:
-            left = right
-
-        if prices[right] > prices[left] and profit_so_far < prices[right] - prices[left]:
-            profit_so_far = prices[right] - prices[left]
-
-        right += 1
-
-    return profit_so_far
-    """
-
     min_so_far = prices[0]
     profit = 0
 
-    for i in prices[1:]:
-        if i < min_so_far:
-            min_so_far = i
-        if profit < i - min_so_far:
-            profit = i - min_so_far
+    for price in prices[1:]:
+        min_so_far = min(price, min_so_far)
+        profit = max(profit, price - min_so_far)
 
     return profit
 
 
 prices = [7, 1, 5, 3, 6, 4]
-print(maxProfit(prices))
+print('The maximum profit that can be obtained is: ', maxProfit_2Pointers(prices))
 
 """
 Time Complexity: O(n)
