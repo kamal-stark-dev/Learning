d02d9a94e6df58017993d87593abfb13abedb71f
kamal-stark-dev <kamalveer356@gmail.com>
2025-11-30 18:25:14 +0530

    improve the code

--- LeetCode/237. Implementing Binary Tree.cpp
+++ LeetCode/237. Implementing Binary Tree.cpp
@@ -21,6 +21,13 @@ class BinaryTree {
 private:
     TreeNode *root;
 
+    void destroy(TreeNode* node) {
+        if (!node) return;
+        destroy(node->left);
+        destroy(node->right);
+        delete node;
+    }
+
     void displayHelper(TreeNode* root) {
         if (!root) return;
         displayHelper(root->left);
@@ -38,7 +45,13 @@ private:
         return searchHelper(root->right, data);
     }
 public:
+    // constructor
     BinaryTree(): root(nullptr) {}
+
+    // destructor
+    ~BinaryTree() {
+        destroy(root);
+    }
 
     void insertNode(int data) {
         if (!root) {
@@ -59,6 +72,21 @@ public:
 
     void display() {
         displayHelper(root);
+        cout << "\n";
+    }
+
+    void levelOrderDisplay() {
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* curr = q.front(); q.pop();
+            if (curr) {
+                cout << curr->val << " ";
+                if (curr->left) q.push(curr->left);
+                if (curr->right) q.push(curr->right);
+            }
+        }
         cout << "\n";
     }
 
@@ -120,33 +148,43 @@ int main(void) {
     srand(static_cast<unsigned int>(time(0)));
 
     // Binary Tree
-    BinaryTree *bt = new BinaryTree();
+    BinaryTree bt;
 
     // insertion
-    bt->insertNode(4);
-    bt->insertNode(1);
-    bt->insertNode(2);
-    bt->insertNode(3);
-    bt->insertNode(5);
-    bt->insertNode(8);
+    bt.insertNode(4);
+    bt.insertNode(1);
+    bt.insertNode(2);
+    bt.insertNode(3);
+    bt.insertNode(5);
+    bt.insertNode(8);
 
     // display
-    bt->display();
+    bt.display();
 
     // searching
-    cout << ((bt->searchNode(3) != nullptr) ? "true": "false") << "\n";
-    cout << ((bt->searchNode(10) != nullptr) ? "true": "false") << "\n";
+    cout << ((bt.searchNode(3) != nullptr) ? "true": "false") << "\n";
+    cout << ((bt.searchNode(10) != nullptr) ? "true": "false") << "\n";
 
     // deletion
-    bt->deleteNode(2);
-    bt->deleteNode(8);
+    bt.deleteNode(2);
+    bt.deleteNode(8);
 
-    bt->display();
+    bt.display();
+
+    // level order traversal
+    bt.levelOrderDisplay();
 
     return 0;
 }
 
 /*
-Time Complexity: O()
-Space Complexity: O()
+Time Complexity:
+    - insertNode: O(h) average, O(n) worst
+    - searchNode: O(n)
+    - deleteNode: O(n)
+    - display   : O(n)
+
+Space Complexity:
+    - deleteNode: O(n) (queue)
+    - recursion-based-functions: O(h) call stack
 */
