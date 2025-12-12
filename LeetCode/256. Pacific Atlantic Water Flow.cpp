fef64e8067bc93065e2599ddf8cd78137a193d31
kamal-stark-dev <kamalveer356@gmail.com>
2025-12-12 22:24:26 +0530

    add bfs approach

--- LeetCode/256. Pacific Atlantic Water Flow.cpp
+++ LeetCode/256. Pacific Atlantic Water Flow.cpp
@@ -71,3 +71,69 @@ int main(void) {
 Time Complexity: O(m * n)
 Space Complexity: O(m * n), pac and atl arrays
 */
+
+// below is the BFS approach for the same (better because it's iterative - prevents recursion depth issues)
+
+/*
+class Solution {
+private:
+    void bfs(queue<pair<int, int>>& q, vector<vector<bool>>& visit, vector<vector<int>>& heights) {
+        int rows = heights.size(), cols = heights[0].size();
+        static int dirs[5] = {-1, 0, 1, 0, -1};
+
+        while (!q.empty()) {
+            auto [r, c] = q.front(); q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dirs[i];
+                int nc = c + dirs[i + 1];
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && !visit[nr][nc] && heights[nr][nc] >= heights[r][c]) {
+                    visit[nr][nc] = true;
+                    q.push({nr, nc});
+                }
+            }
+        }
+    }
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int rows = heights.size(), cols = heights[0].size();
+        if (rows == 0 || cols == 0) return {};
+
+        vector<vector<bool>> pac(rows, vector<bool>(cols, false)),
+                            atl(rows, vector<bool>(cols, false));
+        queue<pair<int, int>> qPac, qAtl;
+
+        // pacific left and top
+        for (int r = 0; r < rows; r++) {
+            qPac.push({r, 0});
+            pac[r][0] = true;
+        }
+        for (int c = 0; c < cols; c++) {
+            qPac.push({0, c});
+            pac[0][c] = true;
+        }
+
+        // atlantic right and bottom
+        for (int r = 0; r < rows; r++) {
+            qAtl.push({r, cols - 1});
+            atl[r][cols - 1] = true;
+        }
+        for (int c = 0; c < cols; c++) {
+            qAtl.push({rows - 1, c});
+            atl[rows - 1][c] = true;
+        }
+
+        // BFS for each ocean
+        bfs(qPac, pac, heights);
+        bfs(qAtl, atl, heights);
+
+        vector<vector<int>> res;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (pac[r][c] && atl[r][c]) res.push_back({r, c});
+            }
+        }
+        return res;
+    }
+};
+*/
