37a6629160ebd453d1ee912a39681bcf4852e259
kamal-stark-dev <kamalveer356@gmail.com>
2025-12-20 14:49:55 +0530

    add learn with kamalveer intro

--- Python/Manim/hello_manim.py
+++ Python/Manim/hello_manim.py
@@ -6,4 +6,18 @@ class HelloManim(Scene):
         self.play(Write(text))
         self.wait()
 
-#  manim -pqh hello_manim.py HelloManim
+class HelloWorld(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square(stroke_color=GREEN)
+        triangle = Triangle().shift(UR)
+
+        text = Text("Learn with Kamalveer!", font_size=42)
+
+        self.play(Write(text, run_time=3))
+        self.play(text.animate.to_edge(UP, buff=0.9), run_time=1.5)
+        self.play(Create(circle))
+        self.play(Create(square))
+        self.play(square.animate.shift(DL))
+        self.play(Transform(text[-1], triangle, run_time=1.5))
+        self.wait()
