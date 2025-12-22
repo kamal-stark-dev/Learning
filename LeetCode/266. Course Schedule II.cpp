7d7edc07f5b9984f04a550a757d291a8bc026940
kamal-stark-dev <kamalveer356@gmail.com>
2025-12-22 21:03:31 +0530

    add kahn's implementation

--- LeetCode/266. Course Schedule II.cpp
+++ LeetCode/266. Course Schedule II.cpp
@@ -1,8 +1,10 @@
 // 210. Course Schedule II
 
+#include <algorithm>
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <queue>
 using namespace std;
 
 bool dfs(int i, vector<int>& order, unordered_map<int, bool>& visited, vector<vector<int>>& adj) {
@@ -35,11 +37,45 @@ vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
     return order;
 }
 
-int main(void) {
-    int numCourses = 2;
-    vector<vector<int>> prerequisites = {{0, 1}};
+// Kahn's Algorithm
 
-    vector<int> order = findOrder(numCourses, prerequisites);
+vector<int> findOrder_Kahn(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> adj(numCourses);
+    vector<int> inDegree(numCourses, 0);
+    for (auto& it: prerequisites) {
+        adj[it[0]].push_back(it[1]);
+        inDegree[it[1]]++;
+    }
+
+    queue<int> q;
+    for (int i = 0; i < numCourses; i++) {
+        if (inDegree[i] == 0) q.push(i);
+    }
+
+    vector<int> order;
+    while (!q.empty()) {
+        int node = q.front(); q.pop();
+        order.push_back(node);
+        for (int nei: adj[node]) {
+            inDegree[nei]--;
+            if (inDegree[nei] == 0) {
+                q.push(nei);
+            }
+        }
+    }
+
+    if (order.size() == 0) return {};
+    reverse(order.begin(), order.end());
+    return order;
+}
+
+int main(void) {
+    // int numCourses = 2;
+    int numCourses = 6;
+    // vector<vector<int>> prerequisites = {{0, 1}};
+    vector<vector<int>> prerequisites = {{2, 0}, {1, 0}, {0, 3}, {3, 4}, {3, 5}};
+
+    vector<int> order = findOrder_Kahn(numCourses, prerequisites);
     for (int course: order) {
         cout << course << " -> ";
     }
