f6f63e304bab12e8ecdbed85513285712eb924be
kamal-stark-dev <kamalveer356@gmail.com>
2025-12-26 15:59:09 +0530

    add coin change

---
+++ LeetCode/271. Coin Change.cpp
@@ -0,0 +1,58 @@
+// 322. Coin Change
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Brute - TC: O(n ^ (amount / m)), where m -> min(coins)
+void dfs(int amount, int count, int& ans, vector<int>& coins) {
+    if (amount == 0) {
+        ans = min(ans, count);
+        return;
+    }
+    if (amount < 0) return;
+
+    int n = coins.size();
+    for (int i = 0; i < n; i++) {
+        dfs(amount - coins[i], count + 1, ans, coins);
+    }
+}
+
+int coinChange_brute(vector<int>& coins, int amount) {
+    int ans = 100001;
+    dfs(amount, 0, ans, coins);
+    return ans;
+}
+
+// tabulation
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, amount + 1);
+    dp[0] = 0;
+
+    for (int curr = 1; curr < amount + 1; curr++) {
+        for (int coin: coins) {
+            if (curr - coin >= 0) {
+                dp[curr] = min(dp[curr], 1 + dp[curr - coin]);
+            }
+        }
+    }
+
+    return (dp[amount] == amount + 1) ? -1 : dp[amount];
+}
+
+int main(void) {
+    vector<int> coins = {1, 2, 5};
+    int amount = 11;
+
+    cout << coinChange(coins, amount) << "\n";
+
+    return 0;
+}
+
+/*
+n -> length of coins
+a -> amount
+
+Time Complexity: O(a * n)
+Space Complexity: O(a)
+*/
