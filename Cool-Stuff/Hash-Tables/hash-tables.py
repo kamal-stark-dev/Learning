4ad46bd8c7a1b49c65918f133bb3ab3e07223b45
kamal-stark-dev <kamalveer356@gmail.com>
2025-12-28 19:35:17 +0530

    add collision handling theory and remove function

--- Cool-Stuff/Hash-Tables/hash-tables.py
+++ Cool-Stuff/Hash-Tables/hash-tables.py
@@ -1,6 +1,8 @@
 # Hash Tables go brrr...
 import random
 
+print("Hash Tables go brrr...\n")
+
 def Hash_Int(n: int) -> int:
     return ((n * n) - (6 * n) + 9) % 10
 
@@ -11,13 +13,13 @@ def Hash_String(s: str) -> int:
     return sum % 50
 
 nums = [5,  0, -7, 8, 100]
-print("Integers:")
+print("Integers Hashes:")
 for num in nums:
     print(Hash_Int(num), end=" ")
 
 
 strs = ["hello", "bam", "cool", "BOOM", ""]
-print("\nStrings:")
+print("\n\nStrings Hashes:")
 for s in strs:
     print(Hash_String(s), end=" ")
 
@@ -31,7 +33,7 @@ def hash_group(name: str, age: int, gender: str) -> int:
     n //= 2 if gender == "M" else 1
     return n % 6
 
-print("\nGroup Keys:")
+print("\n\nGroup Hashes:")
 for name, age, gender in zip(names, ages, genders):
     print(hash_group(name, age, gender), end=" ")
 print()
@@ -53,6 +55,7 @@ A hash function H(x) must be deterministic, this means if H(x) = k, then H(x) mu
 def nd_hash(n):
     return ( n + random.randint(1, 6) ) % 13# die roll
 
+print('\nNon-Deterministic Hash Function:')
 print('H(7):', nd_hash(7))
 print('H(7):', nd_hash(7))
 
@@ -92,18 +95,54 @@ class hashTable:
     def hash(self, x):
         return (x * x + 3) % self.SIZE
 
-    def insert(self, pair):
-        key, val = pair
-        self.table[self.hash(key)] = pair
+    def insert(self, key, val):
+        self.table[self.hash(key)] = (key, val)
 
     def getVal(self, key):
-        return self.table[self.hash(key)][1]
+        pair = self.table[self.hash(key)]
+        return pair[1] if pair else None
+
+    def remove(self, key):
+        self.table[self.hash(key)] = ()
 
 ht = hashTable()
-names = ["Ussop", "Luffy", "Nami", "Sanji", "Zoro", "Chopper"]
+names = ["Luffy", "Zoro", "Nami", "Ussop", "Sanji", "Chopper"]
 for i, name in enumerate(names):
-    ht.insert((i, name))
+    ht.insert(i, name)
 
-print('Hash Table:', ht.table)
+print("\nHash Table:", ht.table)
+print("\nKey 4:", ht.getVal(4))
+ht.remove(4)
+print("Key 4 (after removal):", ht.getVal(4))
 
-print(ht.getVal(4))
+ht2 = hashTable()
+names = ["Luffy", "Zoro", "Nami", "Ussop", "Sanji", "Chopper", "Robin", "Brook", "Jimbei", "Franky"]
+for i, name in enumerate(names):
+    ht2.insert(i, name)
+
+print("\nHash Table 2:", ht2.table)
+
+"""
+But how do we manage collisions in the hash table?
+> currently it'll just override our previous pair with the new pair on the place of collision. (we can see that in our hash table 2)
+
+`Hash Table 2: [(), (), (7, 'Brook'), (0, 'Luffy'), (9, 'Franky'), (), (), (8, 'Jimbei'), (5, 'Chopper'), (6, 'Robin')]`
+We can notice that 4 values are overridden -> 'Zoro', 'Nami', 'Ussop', 'Sanji' the OG strawhats are missing :(
+These names are overridden due to collisions on those indexes which occured due to hash function yielding the same key for 2 values.
+
+We can also notice that 4 values in our hash table are empty, even after inserting values that the structure can hold. It happens due to our hash function being non-uniform (making it a poor hash function). -> which leads to more collisions
+
+
+# Collision Handling:
+
+1. Separate Chaining: deals with hash collisions by maintaining a data structure (usually a linked list) to hold all the different values which hashed to a particular value.
+
+2. Open Addressing: deals with hash collisions by finding another place within the hash table for the object to go by offsetting it from the position to which it hashed to.
+
+
+# Complexity:
+Operation           Average             Worst
+Insertion           O(1)                O(n)
+Removal             O(1)                O(n)
+Search              O(1)                O(n)
+"""
