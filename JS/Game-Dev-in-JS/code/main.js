98e0cd4236cde2e5ecff8970d82bcc6894e3495d
kamal-stark-dev <kamalveer356@gmail.com>
2026-04-02 22:46:11 +0530

    add first mini game

--- JS/Game-Dev-in-JS/code/main.js
+++ JS/Game-Dev-in-JS/code/main.js
@@ -1,32 +1,75 @@
 import kaplay from "kaplay";
 
-kaplay();
-loadBean();
-setGravity(1600);
+const k = kaplay({
+  width: 1280,
+  height: 720,
+});
 
-add([
-  rect(width(), height()),
-  outline(4),
-  area(),
-  pos(0, 0),
-  z(0),
-  color(135, 186, 255),
+k.loadBean();
+k.setGravity(1800);
+
+k.add([k.rect(k.width(), k.height()), k.pos(0, 0), k.color(155, 155, 255)]);
+
+const player = k.add([
+  k.sprite("bean"),
+  k.pos(k.center()),
+  k.area(),
+  k.body(),
+  k.offscreen(),
 ]);
 
-const player = add([sprite("bean"), pos(center()), area(), body(), z(10)]);
+player.onKeyPress("space", () => {
+  if (player.isGrounded()) player.jump();
+});
 
-add([
-  rect(width(), 48),
-  outline(4),
-  area(),
-  pos(0, height() - 48),
-  body({ isStatic: true }),
-  z(10),
-  color(124, 252, 0),
+player.onKeyPress("right", () => {
+  player.move(2000, 0);
+});
+
+player.onKeyPress("left", () => {
+  player.move(-2000, 0);
+});
+
+player.onExitScreen(() => {
+  k.go("gameover");
+});
+
+k.scene("gameover", () => {
+  k.add([k.rect(k.width(), k.height()), k.pos(0, 0), k.color(155, 155, 255)]);
+
+  k.add([
+    k.text("Game Over", { size: 64 }),
+    k.pos(500, 300),
+    k.color(255, 0, 0),
+  ]);
+});
+
+k.add([
+  k.rect(k.width(), 200),
+  k.pos(0, k.height() - 200),
+  k.area(),
+  k.body({ isStatic: true }),
+  k.outline(3),
+  k.color(255, 0, 0),
 ]);
 
-onKeyPress("space", () => {
-  if (player.isGrounded()) {
-    player.jump();
-  }
+let counter = 0;
+const counterUI = k.add([k.text("Score: 0"), k.pos(15, 15)]);
+
+k.loop(1, () => {
+  // update counter
+  counter++;
+  counterUI.text = "Score: " + counter;
+
+  const speeds = [300, 500, 800];
+  const currentSpeed = speeds[Math.floor(Math.random() * speeds.length)];
+
+  k.add([
+    k.rect(50, 50),
+    k.pos(1200, 500),
+    k.area(),
+    k.body(),
+    k.outline(3),
+    k.move(k.vec2(-1, 0), currentSpeed),
+  ]);
 });
