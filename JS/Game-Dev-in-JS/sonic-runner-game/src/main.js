51b89793f175ee81a983e439f784d45c080b37d1
kamal-stark-dev <kamalveer356@gmail.com>
2026-04-03 13:20:54 +0530

    add infinite scrolling on bg and platform

--- JS/Game-Dev-in-JS/sonic-runner-game/src/main.js
+++ JS/Game-Dev-in-JS/sonic-runner-game/src/main.js
@@ -1,3 +1,52 @@
 import k from "./kaplayCtx.js";
+import mainMenu from "./scenes/mainMenu.js";
 
+// 1. loading assets
 
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
+k.loadSprite("platforms", "graphics/platforms.png");
+
+// handling spritesheet
+k.loadSprite("sonic", "graphics/sonic.png", {
+  sliceX: 8,
+  sliceY: 2,
+  anims: {
+    run: { from: 0, to: 7, loop: true, speed: 30 },
+    jump: { from: 8, to: 15, loop: true, speed: 100 },
+  },
+});
+
+k.loadSprite("ring", "graphics/ring.png", {
+  sliceX: 16,
+  sliceY: 1,
+  anims: {
+    spin: { from: 0, to: 15, loop: true, speed: 30 },
+  },
+});
+
+k.loadSprite("motobug", "graphics/motobug.png", {
+  sliceX: 5,
+  sliceY: 1,
+  anims: {
+    run: { from: 0, to: 4, loop: true, speed: 8 },
+  },
+});
+
+k.loadFont("mania", "fonts/mania.ttf");
+
+k.loadSound("destroy", "sounds/Destroy.wav");
+k.loadSound("hurt", "sounds/Hurt.wav");
+k.loadSound("hyper-ring", "sounds/HyperRing.wav");
+k.loadSound("jump", "sounds/Jump.wav");
+k.loadSound("ring", "sounds/Ring.wav");
+k.loadSound("city", "sounds/Cing.mp3");
+
+// 2. Creating Scenes
+
+k.scene("main-menu", mainMenu);
+
+k.scene("game", () => {});
+
+k.scene("gameover", () => {});
+
+k.go("main-menu");
---
+++ JS/Game-Dev-in-JS/sonic-runner-game/src/scenes/mainMenu.js
@@ -0,0 +1,41 @@
+import k from "../kaplayCtx";
+
+export default function mainMenu() {
+  if (!k.getData("best-score")) k.setData("best-score", 0);
+  k.onButtonPress("jump", () => k.go("game"));
+
+  const bgPieceWidth = 1920;
+  const bgPieces = [
+    k.add([k.sprite("chemical-bg"), k.pos(0, 0), k.scale(2), k.opacity(0.8)]),
+    k.add([
+      k.sprite("chemical-bg"),
+      k.pos(bgPieceWidth * 2, 0),
+      k.scale(2),
+      k.opacity(0.8),
+    ]),
+  ];
+
+  const platformWidth = 1280;
+  const platforms = [
+    k.add([k.sprite("platforms"), k.pos(0, 450), k.scale(4)]),
+    k.add([k.sprite("platforms"), k.pos(platformWidth * 4, 450), k.scale(4)]),
+  ];
+
+  k.onUpdate(() => {
+    if (bgPieces[1].pos.x < 0) {
+      bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
+      bgPieces.push(bgPieces.shift()); // shift returns the first element and that gets pushed to the back
+    }
+
+    bgPieces[0].move(-100, 0);
+    bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
+
+    if (platforms[1].pos.x < 0) {
+      platforms[0].moveTo(platforms[1].pos.x + platforms[1].width * 4);
+      platforms.push(platforms.shift());
+    }
+
+    platforms[0].move(-4000, 0);
+    platforms[1].moveTo(platforms[0].pos.x + platforms[1].width * 4, 450);
+  });
+}
