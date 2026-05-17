dec1157874cc477ea1ac8b3c874472c08901b896
kamal-stark-dev <kamalveer356@gmail.com>
2026-05-17 17:21:21 +0530

    add raylib python basics

---
+++ Raylib/python/main.py
@@ -0,0 +1,37 @@
+from pyray import *
+from raylib import *
+
+# to make paths valid for every OS
+from os.path import join
+
+init_window(900, 600, "Hello, World!")
+
+BACKGROUND_COLOR = WHITE
+PRIMARY_COLOR = (245, 40, 145, 255)
+
+# import
+text_card_texture = load_texture(join("assets", "text_card.png"))
+
+while not window_should_close():
+    begin_drawing()
+
+    clear_background(BACKGROUND_COLOR)
+
+    # shapes
+    draw_circle(450, 300, 100, PRIMARY_COLOR)
+
+    for radius in range(120, 201, 20):
+        draw_circle_lines(450, 300, radius, PRIMARY_COLOR)
+
+    draw_line_ex([0, 0], [900, 600], 4, PRIMARY_COLOR)
+
+    # textures (images)
+    draw_texture_ex(text_card_texture, [30, 480], 0, 0.04, WHITE)
+
+    # text
+    draw_text("Raylib", 72, 513, 32, WHITE)
+
+
+    end_drawing()
+
+close_window()
